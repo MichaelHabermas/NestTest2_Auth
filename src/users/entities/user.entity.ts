6b9b4f98fda6f093e3961a26bb7d3f7af25b8824
habermoose <habermoose@gmail.com>
2024-01-16 18:49:23 -0500

    update user eps

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -3,6 +3,10 @@ import { User } from '@prisma/client';
 import { Exclude } from 'class-transformer';
 
 export class UserEntity implements User {
+  constructor(partial: Partial<UserEntity>) {
+    Object.assign(this, partial);
+  }
+
   @ApiProperty()
   id: number;
 
