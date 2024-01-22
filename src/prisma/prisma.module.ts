7e98426edda3d62f7cfebeec276fc5ac08dcb805
habermoose <habermoose@gmail.com>
2024-01-21 19:01:41 -0500

    cleanup

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { ConfigService } from '@nestjs/config';
 
+@Global()
 @Module({
   providers: [ConfigService, PrismaService],
   exports: [PrismaService],
