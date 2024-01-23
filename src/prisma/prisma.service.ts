a2218df09371344615f41c9b4ba0868abba57694
habermoose <habermoose@gmail.com>
2024-01-22 21:13:19 -0500

    cleanup

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -5,13 +5,7 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor(config: ConfigService) {
-    super({
-      datasources: {
-        db: {
-          url: config.get('DATABASE_URL'),
-        },
-      },
-    });
+    super({ datasources: { db: { url: config.get('DATABASE_URL') } } });
   }
 
   cleanDb() {
